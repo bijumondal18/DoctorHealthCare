7480fb357fb4b56f18fbdb1656638838fb36548a
bijumondal18@gmail.com <Abcd1994@#>
2020-08-17 03:57:25 +0530

    project structure setup

--- app/src/main/java/com/bijumondal/doctorhealthcare/Constants.kt
+++ app/src/main/java/com/bijumondal/doctorhealthcare/Constants.kt
@@ -1,6 +1,13 @@
 package com.bijumondal.doctorhealthcare
 
 object Constants {
+    const val API_DATE_FORMAT = "yyyy-MM-dd"
+    const val API_TIME_FORMAT = "HH:mm:ss"
+    const val API_CONTENT_TYPE = "application/json"
 
+    const val API_BASE_URL = "http://103.121.157.2:6192/index.php/"
 
+    //?XDEBUG_SESSION_START=netbeans-xdebug
+
+    const val VALIDATE_MOBILE_NUMBER_URL = "consumer/users/validate"
 }
